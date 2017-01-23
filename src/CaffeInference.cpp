10a4c1a6afaf8e86f543d460c4fb1982554d3ded
Thomio Watanabe <thomio.watanabe@gmail.com>
2017-01-23 17:26:28 -0200

    Create CaffeInference printImageInfo() function

--- src/CaffeInference.cpp
+++ src/CaffeInference.cpp
@@ -7,6 +7,8 @@
 
 using namespace std;
 
+using cv::Mat;
+
 using caffe::Caffe;
 using caffe::Blob;
 using caffe::Net;
@@ -15,6 +17,7 @@ using caffe::NetParameter;
 using caffe::LayerParameter;
 using caffe::TransformationParameter;
 using caffe::DataTransformer;
+
 
 
 CaffeInference::CaffeInference( const string &model_file, const string &trained_file ) : imageBlobName("images"), stateBlobName("states"), outputBlobName("output")
@@ -73,12 +76,33 @@ void CaffeInference::printBlobInfo( const string &blob_name, const Blob<float> &
 }
 
 
+void CaffeInference::printImageInfo( const Mat &image ) const
+{
+    cout << "Depth = " << image.depth() << endl;
+    cout << "Number of channels = " << image.channels() << endl;
+    cout << "Number of rows = " << image.rows << endl;
+    cout << "Number of collumns = " << image.cols << endl;
+}
+
+
 void CaffeInference::printBlobContent( const string &blob_name, Blob<float> &blob ) const
 {
     cout << "Blob: " << blob_name << endl;
     const float *content = blob.cpu_data();
     for( size_t i = 0; i < blob.count(); ++i )
         cout << i << " = " << content[i] << endl;
+}
+
+
+void CaffeInference::printOutput() const
+{
+    boost::shared_ptr<Blob<float> > output_blob = caffeNet->blob_by_name( outputBlobName );
+    printBlobInfo("output_blob", *output_blob);
+
+    const float *output_data = output_blob->cpu_data();
+    for(size_t i = 0; i < output_blob->shape(1); ++i){
+        cout << "Output " << i << " = " << output_data[i] << endl;
+    }
 }
 
 
@@ -91,13 +115,6 @@ void CaffeInference::loadImageMean( const string &mean_file )
     mean_blob.FromProto(mean_proto);
 
     imageMean.CopyFrom(mean_blob, false, true);
-}
-
-
-void CaffeInference::transformInput()
-{
-    Blob<float> input_blob, output_blob;
-    dataTransformer->Transform( &input_blob, &output_blob );
 }
 
 
@@ -121,12 +138,14 @@ void CaffeInference::loadHDF5( const string &file_name, unsigned max_dim )
 }
 
 
-void CaffeInference::feedImage( cv::Mat input_image )
+void CaffeInference::feedImage( const Mat &input_image )
 {
     // Crop image and remove mean
 
     boost::shared_ptr<Blob<float> > image_blob = caffeNet->blob_by_name( imageBlobName );
-    printBlobInfo("image_blob", *image_blob);
+    printBlobInfo( "image_blob", *image_blob );
+
+//    dataTransformer->Transform( input_image, image_blob.get() );
 
     cv::Mat image(image_blob->shape(2), image_blob->shape(3), CV_32FC3, image_blob->mutable_cpu_data() );
     image = input_image / 256.0f;
@@ -148,18 +167,6 @@ void CaffeInference::feedState( const float *input_state )
 }
 
 
-void CaffeInference::printOutput() const
-{
-    boost::shared_ptr<Blob<float> > output_blob = caffeNet->blob_by_name( outputBlobName );
-    printBlobInfo("output_blob", *output_blob);
-
-    const float *output_data = output_blob->cpu_data();
-    for(size_t i = 0; i < output_blob->shape(1); ++i){
-        cout << "Output " << i << " = " << output_data[i] << endl;
-    }
-}
-
-
 void CaffeInference::Predict( cv::Mat input_image, const float *input_state )
 {
     feedImage( input_image );
@@ -168,4 +175,37 @@ void CaffeInference::Predict( cv::Mat input_image, const float *input_state )
     caffeNet->Forward();
 
     printOutput();
+}
+
+
+int main()
+{
+    const string model_file = "./caffe/network/drl_gazebo.prototxt";
+    const string trained_file = "./caffe/models/drl_gazebo_iter_10000.caffemodel";
+
+    CaffeInference drl( model_file, trained_file );
+
+//    const string mean_file = "./caffe/dataset/rl_gazebo_mean.binaryproto";
+//    drl.loadImageMean( mean_file );
+
+//    const string states_file = "./caffe/dataset/train_policy.h5";
+//    drl.loadHDF5( states_file, 6 );
+
+//    const string policy_file = "./caffe/dataset/train_states.h5";
+//    drl.loadHDF5( policy_file, 9 );
+
+
+    // Feed input image to network
+    cv::Mat input_image = cv::imread("00000001.png", CV_LOAD_IMAGE_COLOR );
+//    cv::Mat input_image = cv::imread("00019843.png", CV_LOAD_IMAGE_COLOR );
+    input_image.convertTo(input_image, CV_32FC3);
+
+    // Feed input state to network
+    const float input_state[] = {5, 0, 0, 10, 0, 0, 0, 0, 0};
+//    const float input_state[] = {10, -9, 0, 8, 0, 0, 5, 1, 3};
+
+
+    drl.Predict( input_image, input_state );
+
+    return 0;
 }
--- src/CaffeInference.hpp
+++ src/CaffeInference.hpp
@@ -1,6 +1,11 @@
 #ifndef CAFFE_INFERENCE_HPP_
 #define CAFFE_INFERENCE_HPP_
 
+// Copyright © 2017 Thomio Watanabe
+// Universidade de Sao Paulo
+// Laboratorio de robotica movel
+// January 2017
+
 #include <boost/shared_ptr.hpp>
 #include <string>
 
@@ -14,7 +19,6 @@ class CaffeInference{
                     const std::string &trained_file );
 
     void getTransformation( const std::string &model_file );
-    void transformInput();
 
     void loadHDF5( const std::string &file_name, unsigned max_dim );
     void loadImageMean( const std::string &mean_file );
@@ -26,11 +30,12 @@ class CaffeInference{
     caffe::Blob<float> imageMean;
     const std::string imageBlobName, stateBlobName, outputBlobName;
 
-    void feedImage( cv::Mat input_image );
+    void feedImage( const cv::Mat &input_image );
     void feedState( const float *input_state );
 
     void printOutput() const;
     void printNetInfo() const;
+    void printImageInfo( const cv::Mat &image ) const;
     void printBlobInfo( const std::string &blob_name, const caffe::Blob<float> &blob ) const;
     void printBlobContent( const std::string &blob_name, caffe::Blob<float> &blob ) const;
 };
